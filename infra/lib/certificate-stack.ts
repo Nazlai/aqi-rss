736a750749eda36874221496303c48e4e40d3223
nazlai <joebcrmd@gmail.com>
2026-07-08 16:38:55 +0800

    feat: add acm certificate stack

---
+++ infra/lib/certificate-stack.ts
@@ -0,0 +1,26 @@
+import { App, Stack, StackProps } from "aws-cdk-lib";
+import {
+  Certificate,
+  CertificateValidation,
+} from "aws-cdk-lib/aws-certificatemanager";
+import { domain, domainName } from "./constants";
+import { HostedZone } from "aws-cdk-lib/aws-route53";
+
+export class CertificateStack extends Stack {
+  public readonly certificateArn: string;
+
+  constructor(scope: App, id: string, props: StackProps) {
+    super(scope, id, props);
+
+    const hostedZone = HostedZone.fromLookup(this, "HostedZone", {
+      domainName: domainName,
+    });
+
+    const certificate = new Certificate(this, "AqiRssCertificate", {
+      domainName: domain,
+      validation: CertificateValidation.fromDns(hostedZone),
+    });
+
+    this.certificateArn = certificate.certificateArn;
+  }
+}
