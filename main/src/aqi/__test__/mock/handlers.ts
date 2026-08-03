51b43a38bb4ef30e420d1c0d94bc98efce281e19
nazlai <joebcrmd@gmail.com>
2026-08-03 20:23:08 +0800

    test: pass axios client as params in tests

--- main/src/aqi/__test__/mock/handlers.ts
+++ main/src/aqi/__test__/mock/handlers.ts
@@ -1,7 +1,8 @@
 import { http, HttpResponse } from "msw";
 import aqiMock from "./aqi.mock.json";
 import hourlyAqiMock from "./hourlyaqi.mock.json";
-import { API_ENDPOINT } from "../../../constants/env";
+
+const API_ENDPOINT = process.env.API_ENDPOINT;
 
 export const handlers = [
   http.get(`${API_ENDPOINT}/aqx_p_432`, () => {
