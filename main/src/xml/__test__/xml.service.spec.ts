b0f880197fc121720baf9371bf109fa17f16f428
nazlai <joebcrmd@gmail.com>
2026-08-11 18:43:41 +0800

    test: use iso8601 date string to avoid mismatch between machines

--- main/src/xml/__test__/xml.service.spec.ts
+++ main/src/xml/__test__/xml.service.spec.ts
@@ -11,17 +11,17 @@ describe("xml service", () => {
     <link>https://aqi.tageszeiten.com</link>
     <description>Taiwan Air Quality Index</description>
     <language>en-us</language>
-    <lastBuildDate>Wed, 03 Jun 2026 16:00:00 GMT</lastBuildDate>
+    <lastBuildDate>Thu, 04 Jun 2026 00:00:00 GMT</lastBuildDate>
     <atom:link href="https://aqi.tageszeiten.com/feed.xml" rel="self" type="application/rss+xml"/>
     <item>
       <title>Today&apos;s air quality index in Taiwan</title>
       <link>https://aqi.tageszeiten.com</link>
-      <pubDate>Wed, 03 Jun 2026 16:00:00 GMT</pubDate>
+      <pubDate>Thu, 04 Jun 2026 00:00:00 GMT</pubDate>
       <guid isPermaLink="true">https://aqi.tageszeiten.com</guid>
     </item>
   </channel>
 </rss>`;
 
-    expect(service.create("Thu Jun 04 2026")).toBe(expected);
+    expect(service.create("2026-06-04T00:00:00.000Z")).toBe(expected);
   });
 });
