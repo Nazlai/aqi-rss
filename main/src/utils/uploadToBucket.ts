ff9cdf20c2849b7fab2def05e0392dca47c4e5cc
nazlai <joebcrmd@gmail.com>
2026-08-15 12:21:14 +0800

    refactor: pass ssm client as params to loadEnvironmentVariables

--- main/src/utils/uploadToBucket.ts
+++ main/src/utils/uploadToBucket.ts
@@ -1,9 +1,10 @@
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { loadEnvironmentVariables } from "./loadEnvironmentVariables";
+import { SSM } from "@aws-sdk/client-ssm";
 
 export async function uploadToBucket(filename: string, body: string) {
   const s3Client = new S3Client({});
-  const environmentVariables = await loadEnvironmentVariables();
+  const environmentVariables = await loadEnvironmentVariables(new SSM());
 
   const putObjectCommand = new PutObjectCommand({
     Bucket: environmentVariables.S3_BUCKET_NAME,
